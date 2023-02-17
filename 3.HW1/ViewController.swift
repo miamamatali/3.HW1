c0ad0edbac1172ddccf23ad902d202fae47f61ae
MIA <mamatalievaindira1@gmail.com>
2023-02-17 18:46:26 +0600

    check

--- 3.HW1/ViewController.swift
+++ 3.HW1/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var password: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func login(_ sender: Any) {
+        if userName.text == "" || password.text == "" {
+//            userName.layer.borderColor = UIColor.red.cgColor
+            userName.layer.backgroundColor = UIColor.red.cgColor
+            userName.tintColorDidChange()
+            
+        }
+    }
 }
 
+///Users/user/Desktop/3.HW1/3.HW1/ViewController.swift
